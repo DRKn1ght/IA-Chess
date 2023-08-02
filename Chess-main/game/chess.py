29af98c5d0a652e7abfe2145e659e954837768b1
Guilherme Panobianco <guipanobianc@gmail.com>
2023-08-01 23:17:19 -0300

    .

--- Chess-main/game/chess.py
+++ Chess-main/game/chess.py
@@ -5,7 +5,7 @@ from pygame.locals import *
 from game.piece.new_game import create_white_pieces, create_black_pieces, FEN_to_board
 from piece.new_game import create_white_pieces, create_black_pieces
 from settings import Settings
-from results import Results
+from GUI.results import Results
 from Board.board import Board
 from piece.pawn import Pawn
 from piece.king import King
@@ -27,9 +27,8 @@ class ChessGame:
 
         self.board = Board(self)
 
-        
-        self.white_king, self.white_pieces = create_white_pieces(self)
-        self.black_king, self.black_pieces = create_black_pieces(self)
+        self.white_king, self.white_pieces, self.white_rook_king_side, self.white_rook_queen_side = create_white_pieces(self)
+        self.black_king, self.black_pieces, self.black_rook_king_side, self.black_rook_queen_side = create_black_pieces(self)
 
         self.sound = pygame.mixer.Sound("Assets/chessmove.wav")
         #Board._init_from_FEN(self, "rnbqkbnr/p1pp1p1p/3P4/1p4p1/8/2N1pN2/PPP1PPPP/R1BQKB1R w -")
--- Chess-main/game/piece/new_game.py
+++ Chess-main/game/piece/new_game.py
@@ -15,14 +15,17 @@ def create_white_pieces(ai_game):
         white_pieces.add(Pawn(ai_game, (i,6), "w"))
 
     for i in range(2):
-        white_pieces.add(Rook(ai_game, (7*i,7), "w"))
         white_pieces.add(Knight(ai_game, (5*i+1,7), "w"))
         white_pieces.add(Bishop(ai_game, (3*i+2,7), "w"))
     white_pieces.add(Queen(ai_game, (3,7), "w"))
     white_king = King(ai_game, (4,7), "w")
-    white_pieces.add(white_king)    
+    white_rook_queen_side = Rook(ai_game, (0, 7), "w")
+    white_rook_king_side = Rook(ai_game, (7, 7), "w")
+    white_pieces.add(white_rook_queen_side)
+    white_pieces.add(white_rook_king_side)
+    white_pieces.add(white_king)
 
-    return white_king, white_pieces
+    return white_king, white_pieces, white_rook_queen_side, white_rook_king_side
 
 
 def create_black_pieces(ai_game):
@@ -33,13 +36,18 @@ def create_black_pieces(ai_game):
         black_pieces.add(Pawn(ai_game, (i,1), "b"))
 
     for i in range(2):
-        black_pieces.add(Rook(ai_game, (7*i,0), "b"))
         black_pieces.add(Knight(ai_game, (5*i+1,0), "b"))
         black_pieces.add(Bishop(ai_game, (3*i+2,0), "b"))
+    
     black_pieces.add(Queen(ai_game, (3,0), "b"))
     black_king = King(ai_game, (4,0), "b")
+    black_rook_queen_side = Rook(ai_game, (0, 0), "b")
+    black_rook_king_side = Rook(ai_game, (7, 0), "b")
+    black_pieces.add(black_rook_queen_side)
+    black_pieces.add(black_rook_king_side)
     black_pieces.add(black_king)    
 
+<<<<<<< HEAD
     return black_king, black_pieces
 
 
@@ -87,3 +95,6 @@ def FEN_to_board(ai_game, FEN):
                 black_pieces.add(piece)
             col += 1
     return white_king, white_pieces, black_king, black_pieces
+=======
+    return black_king, black_pieces, black_rook_queen_side, black_rook_king_side
+>>>>>>> parent of ea033e5 (atualizações)
